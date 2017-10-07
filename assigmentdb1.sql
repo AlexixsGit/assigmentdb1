2c8926efdae4b3afac5f95772e8b38005532841b
Edward Alexis Ortiz <e.du1991@hotmail.com>
2017-10-07 16:29:28 -0500

    I created the tablaspaces and datafiles

--- assigmentdb1.sql
+++ assigmentdb1.sql
@@ -1,2 +1,35 @@
 /*START DATABASE*/
-/*hola mundo cruel*/
+
+/*Create tablespaces*/
+create tablespace gofar_travel datafile ',tempDataFile01.dbf' size 500m,'tempDataFile02.dbf' size 300m,'tempDataFile03.dbf' size 224m
+autoextend on maxsize 1024m
+extent management local
+segment space management auto
+online;
+
+create tablespace test_purposes datafile ',tempDataFile04.dbf' size 500m
+autoextend on maxsize 500m
+extent management local
+segment space management auto
+online;
+
+create tablespace test_purposes datafile ',tempDataFile04.dbf' size 500m
+autoextend on maxsize 500m
+extent management local
+segment space management auto
+online;
+
+create tablespace Undo datafile ',tempDataFile05.dbf' size 5m
+autoextend on maxsize 5m
+extent management local
+segment space management auto
+online;
+
+
+SELECT TABLESPACE_NAME "TABLESPACE",
+   INITIAL_EXTENT "INITIAL_EXT",
+   NEXT_EXTENT "NEXT_EXT",
+   MIN_EXTENTS "MIN_EXT",
+   MAX_EXTENTS "MAX_EXT",
+   PCT_INCREASE
+   FROM DBA_TABLESPACES; 
